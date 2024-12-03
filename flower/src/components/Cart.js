aa3c5e83c2c0397de5e508f4fe74b6009dd05cc6
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-12-03 20:48:55 +0530

    done

--- flower/src/components/Cart.js
+++ flower/src/components/Cart.js
@@ -1,5 +1,7 @@
-export default function Cart() {
-    
+import React from 'react';
+export default function Cart({ cartItems }) {
+    const total = cartItems.reduce((acc, item) => acc + item.price * item.qty, 0);
+
     return (
         <div className="table-container">
             <h4 className="card-title">Cart</h4>
@@ -12,11 +14,17 @@ export default function Cart() {
 
                 </thead>
                 {
-                    
+                    cartItems.map(item => (
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>{item.qty}</td>
+                            <td>{item.price * item.qty}</td>
+                        </tr>
+                    ))
                 }
                 <tr>
                     <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
+                    <td colSpan={2}>{total}</td>
                 </tr>
             </table>
            

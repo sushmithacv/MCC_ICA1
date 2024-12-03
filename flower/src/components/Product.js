60f402aeb41f472833a1e805bf4a56ab7f287366
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-12-03 20:32:59 +0530

    img & price displayed

--- flower/src/components/Product.js
+++ flower/src/components/Product.js
@@ -1,5 +1,15 @@
 import '../assets/CSS/layout.css';
-export default function Product({ flower}){
+import React, { useState } from 'react';
+export default function Product({ flower, addToCart}){
+    
+    const [quantity, setQuantity] = useState(0);
+
+    const handleAddToCart = () => {
+        if (quantity > 0) {
+            addToCart(flower, quantity);
+            setQuantity(0); // Reset quantity after adding to cart
+        }
+    };
     
     return(
         <div className="grid-item">
@@ -7,12 +17,12 @@ export default function Product({ flower}){
             <div class="card">
             <img src={require('../assets/image/'+flower.img)} alt={flower.name} />
                 <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
+                    <h5 class="card-title">Price:{flower.price}</h5>
                     <div class="quantity-container">
                         <label for="quantity">Quantity:</label>
                         <input type="number" id="quantity" name="quantity" />
                     </div>
-                    <button class="card-button">Add to Cart</button>
+                    <button class="card-button" onClick={handleAddToCart}>Add to Cart</button>
                 </div>
             </div>
         </div>
--- flower/src/components/Products.js
+++ flower/src/components/Products.js
@@ -1,9 +1,20 @@
 import '../assets/CSS/layout.css';
+import React, { useState } from 'react';
 import Product from './Product';
 import Cart from './Cart';
 import { flowers } from './FlowerDB';
 export default function Products(){
-   
+    const [cartItems, setCartItems] = useState([]);
+
+    const addToCart = (flower, quantity) => {
+        const existingItem = cartItems.find(item => item.id === flower.id);
+        if (existingItem) {
+            existingItem.qty += quantity;
+            setCartItems([...cartItems]);
+        } else {
+            setCartItems([...cartItems, { ...flower, qty: quantity }]);
+        }
+    };
     
     return(
         <>
@@ -16,7 +27,7 @@ export default function Products(){
                     {
                         //product
                         flowers.map(flower => (
-                            <Product key={flower.id} flower={flower}  />
+                            <Product key={flower.id} flower={flower}  addToCart={addToCart}/>
                         ))
                     }
                 </div>

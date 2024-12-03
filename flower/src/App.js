6c2ae4ff5e2ee099b21bd101a042d29eebdc42df
sushmithacv <154046226+sushmithacv@users.noreply.github.com>
2024-12-03 19:39:20 +0530

    Initialized

---
+++ flower/src/App.js
@@ -0,0 +1,12 @@
+import './assets/CSS/layout.css';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <div className="main-grid-container">
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
